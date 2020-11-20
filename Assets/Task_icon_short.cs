b1f083f53fc857f1d6d8caf95fcbc254e04718e5
slimerplanet <eppuku@gmail.com>
2020-11-20 22:13:28 +0200

    maaaaap

---
+++ Assets/Task_icon_short.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task_icon_short : MonoBehaviour
+{
+    public TaskManager manager;
+    
+    public bool swipe;
+    public bool download;
+    public downUpLoadSpot spot;
+
+
+    private void Update()
+    {
+        if(manager.hasCardSwipe && swipe && !manager.cardSwipeCompleted)        
+            gameObject.SetActive(true);
+        else 
+            gameObject.SetActive(false);
+
+        if (spot.hasTaskHere && !manager.downloadCompleted)
+            gameObject.SetActive(true);
+        else
+            gameObject.SetActive(false);
+
+        
+    }
+}
