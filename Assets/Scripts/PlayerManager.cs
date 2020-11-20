a74d0c4af84ba2675d66abd269c1f87e1580e912
slimerplanet <eppuku@gmail.com>
2020-11-20 19:22:43 +0200

    initioal commit

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,45 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : NetworkBehaviour
+{
+    public bool isImpostor;
+    public bool isDead;
+
+    public int color;
+
+    public Texture[] colors;
+    public Texture[] backbagColors;
+
+    public Material bodyMat;
+    public Material backMat;
+
+
+    [Header("networking")]
+    public string remotePlayerLayerName;
+    public GameObject body;
+    public Behaviour[] stufftoDisable;
+
+
+    private void Start()
+    {
+        if(!isLocalPlayer)
+        {
+            for (int i = 0; i < stufftoDisable.Length; i++)
+            {
+                stufftoDisable[i].enabled = false;
+            }
+            body.layer = LayerMask.NameToLayer(remotePlayerLayerName);
+        }
+
+
+    }
+    private void Update()
+    {
+        bodyMat.mainTexture = colors[color];
+        backMat.mainTexture = backbagColors[color];
+    }
+
+}
---
+++ Assets/task_cardSwipe.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class task_cardSwipe : MonoBehaviour
+{
+    public Slider slider;
+    public TaskManager manager;
+    public GameObject panel;
+
+    bool inTrigger;
+
+    bool t = true;
+    private void Update()
+    {
+        if(slider.value == 1)
+        {
+            manager.cardSwipeCompleted = true;
+            panel.SetActive(false);
+            manager.isIntask = false;
+            manager.taskscompleted++;
+            slider.value = 0;
+        }
+        if(inTrigger && Input.GetKeyDown(KeyCode.E) && manager.hasCardSwipe)
+        {
+            panel.SetActive(true);
+        }
+
+
+    }
+
+    public void OpenTask()
+    {
+        manager.isIntask = true;
+        panel.SetActive(true);
+    }
+
+
+
+    
+}
