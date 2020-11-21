f490da53d862d0b084072c25bcfe52e0d576f4fe
slimerplanet <eppuku@gmail.com>
2020-11-21 21:39:07 +0200

    vent

--- AAAAAAAAAA/Assets/vent.cs
+++ AAAAAAAAAA/Assets/vent.cs
@@ -5,5 +5,32 @@ using UnityEngine;
 public class vent : MonoBehaviour
 {
     public Transform exit;
-    
+    public vent_system sys;
+
+    bool inTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("localplayer"))
+        {
+            inTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("localplayer"))
+        {
+            inTrigger = false;
+        }
+    }
+
+    private void Update()
+    {
+        if(inTrigger && GameObject.FindGameObjectWithTag("localplayer").GetComponent<PlayerManager>().isImpostor && Input.GetKeyDown(KeyCode.E))
+        {
+            sys.enterVent();
+        }
+    }
+
 }
--- AAAAAAAAAA/Assets/vent_system.cs
+++ AAAAAAAAAA/Assets/vent_system.cs
@@ -1,17 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class vent_system : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public vent[] vents;
+    public int curVent;
 
-    // Update is called once per frame
-    void Update()
+    public void enterVent()
     {
         
     }
